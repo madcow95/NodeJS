c6e7e1caa39ca77a094abed664e4e956f68dbdfa
madcow95 <lobasketve@naver.com>
2022-09-21 21:36:05 +0900

    주석 정리

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require( "express" );
 const bodyParser = require( "body-parser" );
 const app = express();
+const MongoClient = require( "mongodb" ).MongoClient;
 
 app.use( bodyParser.urlencoded( { extended : true } ) );
 
-const MongoClient = require( "mongodb" ).MongoClient;
 let db;
-MongoClient.connect( '', ( err, client ) => {
+MongoClient.connect( 'mongodb+srv://admin:qwer1234@cluster0.t2fk11g.mongodb.net/?retryWrites=true&w=majority', ( err, client ) => {
     if( err ) {
         return console.log( { err } );
     }
@@ -24,15 +24,11 @@ MongoClient.connect( '', ( err, client ) => {
      * @Param1 : 저장할 데이터
      * @Param2 : Callback Function
      */
-    db.collection( "post" ).insertOne( { name : "madcow", age : 28 }, ( err, res ) => {
-        console.log( "Save Complete" );
-    } );
 
     app.listen( 8080, () => {
         /**
          * Node Server 띄우는 거
          */
-        console.log("DB Connect Success");
     } );
 } );
 
@@ -41,35 +37,20 @@ app.get( "/", ( req, res ) => {
     res.sendFile( `${ __dirname }/html/index.html` );
 } );
 
-/**
- * @Param : req 
- *  => req : {
- *        body : {
- *           name : "data"
- *        }
- *     }
- *  이런 Object로 전송
- */
 app.post( "/add", ( req, res ) => {
-    res.send( "POST Complete" );
+    /**
+     * @Param req : { 
+     *      body : {
+     *           html Tag -> input의 name : '입력 값'
+     *      }
+     *  }
+     * 의 형태로 들어옴
+     */
+    const saveData = {
+        subject : req.body.todo,
+        dueDate : req.body.deadline
+    }
+    db.collection( "post" ).insertOne( saveData, ( err, res ) => {
+        console.log( "Save Completely" );
+    } );
 } );
-
-/**
- * API : Application Programming Interface의 약자
- * 웹 개발에서의 API란?
- * 1. 웹서버와 고객간의 소통방법 : 서버랑 통신할 수 있는 방법
- * 2. 'GET', 'POST'의 첫번째 Parameter ex) '/home'
- * 
- * REST 원칙 6개
- * 1. Uniform Interface : 하나의 자료는 하나의 URL로
- * 2. Client-Server 역할구분 -> 브라우저는 요청 서버는 응답
- * 3. Stateless -> 요청 1과 요청 2는 의존성이 없어야함.
- * 4. Cacheable -> 서버에서 보내주는 정보들은 캐싱이 가능해야함 -> chrome에서 대부분 해주기 때문에 중요하진 않음
- * 5. Layerd System
- * 6. Code on Demand
- * 
- * REST의 좋은예
- * www.example.com/products/66432
- * instagram.com/explore/tags/kpop/
- * facebook.com/natgeo/photos/
- */
