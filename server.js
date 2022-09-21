2fb9124ffc2dd56a69e0c3c544a740adddb17977
madcow95 <lobasketve@naver.com>
2022-09-21 22:06:19 +0900

    9/21

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require( "express" );
 const bodyParser = require( "body-parser" );
-const app = express();
 const MongoClient = require( "mongodb" ).MongoClient;
+const app = express();
 
 app.use( bodyParser.urlencoded( { extended : true } ) );
+app.set( "view engine", "ejs" );
 
 let db;
 MongoClient.connect( 'mongodb+srv://admin:qwer1234@cluster0.t2fk11g.mongodb.net/?retryWrites=true&w=majority', ( err, client ) => {
@@ -15,15 +16,6 @@ MongoClient.connect( 'mongodb+srv://admin:qwer1234@cluster0.t2fk11g.mongodb.net/
      * @Param : db -> MongoDB에서 생성한 db이름인 todoapp에 연결한다.
      */
     db = client.db( "todoapp" );
-
-    /**
-     * collection
-     * @Param : collection이름( 'MongoDB'에서 db생성할 때 입력한 값 )
-     * 
-     * insertOne 
-     * @Param1 : 저장할 데이터
-     * @Param2 : Callback Function
-     */
 
     app.listen( 8080, () => {
         /**
@@ -46,6 +38,15 @@ app.post( "/add", ( req, res ) => {
      *  }
      * 의 형태로 들어옴
      */
+
+    /**
+     * collection
+     * @Param : collection이름( 'MongoDB'에서 db생성할 때 입력한 값 )
+     * 
+     * insertOne 
+     * @Param1 : 저장할 데이터
+     * @Param2 : Callback Function
+     */
     const saveData = {
         subject : req.body.todo,
         dueDate : req.body.deadline
@@ -53,4 +54,17 @@ app.post( "/add", ( req, res ) => {
     db.collection( "post" ).insertOne( saveData, ( err, res ) => {
         console.log( "Save Completely" );
     } );
+} );
+
+app.get( "/list", ( req, res ) => {
+    /**
+     * ejs 파일은 views Dir에 저장하지 않으면 Error: Failed to lookup view 에러 뜸
+     * 
+     * 9/21 : DB에 저장된 post라는 collection의 모든 데이터를 가져옴
+     */
+    db.collection( "post" ).find().toArray( ( err, ressult ) => {
+        console.log(ressult);
+        res.render( "list.ejs", { results : ressult } );
+    } );
+
 } );
